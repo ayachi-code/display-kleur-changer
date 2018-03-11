a607cfbb6a50fbcd116e93eacfee24a303f3b344
bilal <bilal.ayachi@hetmml.nl>
2018-03-11 09:19:37 +0000

    zelfde gedaan bij groen en blauw

--- public/index.js
+++ public/index.js
@@ -2,3 +2,17 @@ let rood_b = document.getElementById("rood").addEventListener("mouseover", () =>
 	console.log("scherm word rood")
 
 })
+
+
+let groen_b = document.getElementById("groen").addEventListener("mouseover", () => {
+        console.log("scherm word groen")
+
+})
+
+
+
+let blauw_b = document.getElementById("blauw").addEventListener("mouseover", () => {
+        console.log("scherm word blauw")
+
+})
+                                                                                                                             
