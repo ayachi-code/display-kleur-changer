1740eb61b0d6f74e946c4e83a26e1b9e347d09ea
bilal <bilal.ayachi@hetmml.nl>
2018-03-11 09:44:41 +0000

    rood is 255

--- server.js
+++ server.js
@@ -13,13 +13,15 @@ const socket = require("socket.io");
 const io = socket(server)
 
 
+let rood = [255,0,0];
+
 io.sockets.on("connection",(socket) => {
 console.log("hey nieuw");
 
  socket.on("rood_muis",(data) => {
 	console.log("Scherm word root")
 
-	
+		
 
 	});
 
